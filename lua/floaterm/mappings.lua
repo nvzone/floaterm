4a316166505e5c141fd6d2d2fe1c58cdcbc6f0d1
siduck <siduck@tutanota.com>
2025-09-22 11:28:52 +0530

    feat: <C-L> mapping to switch to main term from sidebar #24

--- lua/floaterm/mappings.lua
+++ lua/floaterm/mappings.lua
@@ -6,6 +6,7 @@ return function()
   map("n", "e", api.edit_name, { buffer = state.sidebuf })
   map("n", "a", api.new_term, { buffer = state.sidebuf })
   map("n", "d", api.delete_term, { buffer = state.sidebuf })
+  map("n", "<C-l>", api.switch_wins, { buffer = state.sidebuf })
 
   if state.config.mappings.sidebar then
     state.config.mappings.sidebar(state.sidebuf)
